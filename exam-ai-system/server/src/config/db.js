e9c1c85f0494940f9d289ebf4c9d6718e154333b
Aayush Jung Kunwar <paban.piyush@gmail.com>
2026-04-26 14:40:47 +0545

    Create db.js

---
+++ exam-ai-system/server/src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { env } from "./env.js";
+
+export const connectDatabase = async () => {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(env.mongoUri, {
+    autoIndex: env.nodeEnv !== "production",
+  });
+};
+
