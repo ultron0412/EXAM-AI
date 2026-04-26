85fee3e19538548efefe86a598f5ea2fad829925
Aayush Jung Kunwar <paban.piyush@gmail.com>
2026-04-26 14:41:03 +0545

    Create User.js

---
+++ exam-ai-system/server/src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 80,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "admin"],
+      default: "student",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
+
